bd19ae0f463fafc17bf028c7404f04c484de5f28
Vladimir <krasniy.vladim@yandex.ru>
2022-09-20 22:53:02 +0300

    use cors

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,15 @@ import {bloggersRouter} from "./routes/bloggers-router";
 import {postsRouter} from "./routes/posts-router";
 import {testingRouter} from "./routes/testing-router";
 import {basicAuthorization} from "./middlewares/auth-middleware";
+import cors from "cors";
 
 const app = express()
 
 const port =  process.env.PORT || 5000
 
-const parserMiddleware = bodyParser()
+app.use(cors())
+// const parserMiddleware = bodyParser()
+const parserMiddleware = express.json()
 app.use(parserMiddleware)
 
 app.use('/bloggers', bloggersRouter)
