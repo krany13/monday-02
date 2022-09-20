22c7eda98bd8de6ccc969f9e062009043d824229
Vladimir <krasniy.vladim@yandex.ru>
2022-09-20 14:12:51 +0300

    deleteAll

--- src/routes/testing-router.ts
+++ src/routes/testing-router.ts
@@ -1,10 +1,12 @@
 import {Request, Response, Router} from "express";
 import {postsRepository} from "../repositories/posts-repository";
+import {bloggersRepository} from "../repositories/bloggers-repository";
 
 export const testingRouter = Router({})
 
 
 testingRouter.delete('/all-data', (req: Request, res:Response) => {
     const isAllDelete = postsRepository.deleteAllPosts()
+    const isAllDeleted = bloggersRepository.deleteAllBlogs()
     res.sendStatus(204)
 })
